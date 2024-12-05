a41fe5c444296c6f42c18b8c1210fe0df901c0c4
Rasim <90181726+rasimishere@users.noreply.github.com>
2024-12-05 13:58:02 +0400

    Implement Car class

--- vrm.cpp
+++ vrm.cpp
@@ -6,3 +6,15 @@ public:
     virtual double calculateRentalFee(int days) const = 0;
     virtual ~Vehicle() {}
 };
+
+class Car : public Vehicle {
+private:
+    double dailyRate;
+
+public:
+    Car(double rate) : dailyRate(rate) {}
+
+    double calculateRentalFee(int days) const override {
+        return days * dailyRate;
+    }
+};
